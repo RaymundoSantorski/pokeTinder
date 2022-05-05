fddcf90b3ed7f3d5b72704f2cf36c8e95d67d3f8
Raymundo Mateos <rayma9829@gmail.com>
2022-05-05 00:37:39 -0500

    (upload) Se muestran los nombres de los pokemones en pantalla

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,40 @@
+import { 
+    useState, 
+    useEffect
+} from 'react';
+
+import axios from '../../helpers/axiosConfig';
+
 
 export const Main = () => {
+
+  const [pokemons, setPokemons] = useState();
+
+  const getPokeData = ( url ) => {
+    axios.get(url)
+    .then(response => {
+      console.log(response.data);
+    })
+  }
+
+  useEffect(() => {
+    axios.get('/pokemon/')
+    .then(response => {
+      setPokemons(response.data.results);
+    });
+  }, [])
+
   return (
-    <div>Main</div>
+    <>
+      {
+        pokemons ? 
+        pokemons.map(poke => {
+          return (
+            <p>{ poke.name }</p>
+          )
+        })
+        : <p>No hay nada que mostrar</p>
+      }
+    </>
   )
 }
